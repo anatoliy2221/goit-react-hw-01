f4ba5603e3dddf996f25966d7594ff83446cc072
Anatoliy <anatoliy2221@gmail.com>
2024-03-15 23:54:24 +0200

    friend

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Profile from './components/Profile/Profile';
-import FriendList from './components/friendList/FriendList';
+import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from './components/transactionHistory/TransactionHistory';
 import userData from './userData.json';
 import friends from './friends.json';
